f98f074e89acd63e124d45cd01156a85b58f89f1
Saptak P <56467712+saptak-patil@users.noreply.github.com>
2021-11-09 17:56:19 +0530

    javaTest class file
    Git Demo java test class

---
+++ javaTest.java
@@ -0,0 +1,12 @@
+class javaTest {
+
+  public static void main(String args[]){
+  
+    System.out.println("Java Test Class");
+    System.out.println("----------------");
+    
+    System.out.println("GIT Demo");
+  
+  }
+
+}
